a980545e5022540800c8959fc2c3ce6265f2c845
IlmanGifari <66061482+ilman79@users.noreply.github.com>
2023-08-12 10:27:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,6 @@ if selected == "Siswa":
     key_dict = json.loads(st.secrets["textkey"])
     creds = service_account.Credentials.from_service_account_info(key_dict)
     db = firestore.Client(credentials=creads, project="e-statistics2023")
-    client = storage.Client.from_service_account_json("https://firebasestorage.googleapis.com/v0/b/test-json-qurey.appspot.com/o/key.json?alt=media&token=be7f7af6-1460-4e0f-9759-dc0d1f8d97c0")
-    bucket = client.bucket("e-statistics2023.appspot.com")
-
     def get_db():
         db = firestore.Client(credentials=creds, project="e-statistics2023")
         return db 
