15db0af19dfa927ba44d8999ce12c04fd3322678
IlmanGifari <66061482+ilman79@users.noreply.github.com>
2023-08-12 10:36:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -330,6 +330,7 @@ if selected == "Guru" :
     key_dict = json.loads(st.secrets["textkey"])
     creds = service_account.Credentials.from_service_account_info(key_dict)
     db = firestore.Client(credentials=creds, project="e-statistics2023")
+    bucket = storage.bucket()
     def get_db():
         db = firestore.Client(credentials=creds, project="e-statistics2023")
         return db
